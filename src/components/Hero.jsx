8a4f0d881f40f169b459afa2dee22ad3abecc569
Dylan <weinac_d@etna-alternance.net>
2023-05-09 00:24:56 +0200

    desactiver le curseur qui sert pas a grand chose et que je trouve moche

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,9 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      {/* le truc en dessous c'est un curseur pour descendre sur la page mais ca pue la merde */}
+
+      {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
@@ -43,7 +45,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
